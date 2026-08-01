0ddd89f7feda826ce953cbdb2c46efd2bf64dcd7
Swarup Satish Chaudhari <chaudhariswarup266@gmail.com>
2026-08-01 08:22:07 +0530

    Add files via upload

--- Sum.java
+++ Sum.java
@@ -4,12 +4,46 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        float num1 = input.nextFloat();
-        float num2 = input.nextFloat();
+//        int ans = sum2();
+//        System.out.println(ans);
 
-        float sum = num1 + num2;
-
-        System.out.println("Sum = " + sum);
+        int ans = sum3(20, 30);
+        System.out.println(ans);
     }
+
+    // pass the value of numbers when you are calling the method in main()
+    static int sum3(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    // return the value
+    static int sum2() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        return sum;
+//        System.out.println("This will never execute");
+    }
+
+    static void sum() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+    }
+
+    /*
+        return_type name (arguments) {
+            // body
+            return statement;
+        }
+
+     */
 }
